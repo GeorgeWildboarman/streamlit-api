de388ea0a4fadd593f4e894f9a8e4adb625ceab4
GeorgeWildboarman <64960316+GeorgeWildboarman@users.noreply.github.com>
2021-12-31 17:05:07 +0900

    Update data_input_streamlit.py

--- data_input_streamlit.py
+++ data_input_streamlit.py
@@ -4,3 +4,6 @@ your_name = st.text_input("Enter your name", max_chars=10)
 st.title(your_name)
 
 number = st.number_input("Enter number", 0, 25, 6, 2)
+
+my_date = st.date_input("Select date") 
+st.write(my_date)
