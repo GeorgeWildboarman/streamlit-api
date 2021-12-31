0aca31e8522e8c563e5bbc72640752e04dc38e5a
GeorgeWildboarman <64960316+GeorgeWildboarman@users.noreply.github.com>
2021-12-31 17:03:56 +0900

    Update data_input_streamlit.py

--- data_input_streamlit.py
+++ data_input_streamlit.py
@@ -3,8 +3,4 @@ import streamlit as st
 your_name = st.text_input("Enter your name", max_chars=10)
 st.title(your_name)
 
-number0 = st.number_input("Enter number")
-
-number1 = st.number_input("Enter number")
-
-number2 = st.number_input("Enter number", 0, 25, 6, 2)
+number = st.number_input("Enter number", 0, 25, 6, 2)
