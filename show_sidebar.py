59126762b82cd1b6b6dced6ce579225f90a10ab4
GeorgeWildboarman <64960316+GeorgeWildboarman@users.noreply.github.com>
2022-01-09 17:31:50 +0900

    Update show_sidebar.py

--- show_sidebar.py
+++ show_sidebar.py
@@ -19,15 +19,8 @@ main_dsp.header('Display')
 # OSC settings
 h_pos = st.slider('Horizontal position', min_value=-1.0, max_value=1.0, )
 
-time_per_div = 1.0e-5
 point_per_div = 25
 total_div = 10
-time_per_point = time_per_div / point_per_div
-total_point = total_div * point_per_div
-
-
-
-
 
 col1, col2, col3 = st.columns(3)
 
@@ -37,6 +30,7 @@ df_vol = pd.DataFrame({
 })
 
 dict_vol ={'5V': 5, '2V': 2, '1V': 1, '500mV': .5, '200mV': .2, '100mV': .1, '50mV': .05, '20mV': .02}
+
 dict_time = {'25ms': 2.5e-2, '10ms': 1e-2, '5ms': 5e-3, 
              '2.5ms': 2.5e-3, '1ms': 1e-3, '500μs': 5e-4, 
              '250μs': 2.5e-4, '100μs': 1e-4, '50μs': 5e-5, 
@@ -57,10 +51,14 @@ with col2:
   
 with col3:
 #   st.header('TIME')
-  time_ind = st.selectbox('TIME/DIV', dict_time, 4)
-  time_per_div1 = dict_time.get(time_ind)
+  time_ind = st.selectbox('TIME/DIV', dict_time, 10)
+  time_per_div = dict_time.get(time_ind)
   st.write(time_per_div1)
-  
+
+# time_per_div = 1.0e-5
+time_per_point = time_per_div / point_per_div
+total_point = total_div * point_per_div
+
 
 # Create Waveforms
 x = np.arange(total_point+1)
