f72a9cf8a955fb28d1f0dff42cae308f4cddaf6c
GeorgeWildboarman <64960316+GeorgeWildboarman@users.noreply.github.com>
2022-01-11 17:03:36 +0900

    Update osc_demo.py

--- osc_demo.py
+++ osc_demo.py
@@ -1,11 +1,12 @@
-import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
 
 st.title('Oscilloscope')
 
 arr = np.random.normal(1, 1, size=100)
-# fig, ax = plt.subplots()
-# ax.hist(arr, bins=20)
+fig, ax = plt.subplots()
+ax.hist(arr, bins=20)
 
-# st.pyplot(fig)
+st.pyplot(fig)
