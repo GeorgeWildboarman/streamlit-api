891ae584d8181a85470e9ce2accb10d9ffdd5850
GeorgeWildboarman <64960316+GeorgeWildboarman@users.noreply.github.com>
2022-01-11 15:47:10 +0900

    Update show_sidebar.py

--- show_sidebar.py
+++ show_sidebar.py
@@ -2,12 +2,9 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 # import base64 
-import altair as alt
-
+# import altair as alt
 # import plotly.figure_factory as ff
-
 import plotly.figure_factory as ff
-
 ff.graph_objects = go
 
 # import plotly.express as px
