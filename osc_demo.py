e5cd4850dc12e7822036863cfa812cb60ee297bd
GeorgeWildboarman <64960316+GeorgeWildboarman@users.noreply.github.com>
2022-01-11 16:14:18 +0900

    Update osc_demo.py

--- osc_demo.py
+++ osc_demo.py
@@ -1,11 +1,11 @@
 import streamlit as st
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import numpy as np
 
 st.title('Oscilloscope')
 
 arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
+# fig, ax = plt.subplots()
+# ax.hist(arr, bins=20)
 
-st.pyplot(fig)
+# st.pyplot(fig)
