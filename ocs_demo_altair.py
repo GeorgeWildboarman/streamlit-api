04c38340d7140a3c69708f8db7203ef2fc000a8b
GeorgeWildboarman <64960316+GeorgeWildboarman@users.noreply.github.com>
2022-01-15 16:07:43 +0900

    Update ocs_demo_altair.py

--- ocs_demo_altair.py
+++ ocs_demo_altair.py
@@ -18,9 +18,8 @@ def strtime_now_jst():
 def h_point_array(h_total_point):
   return np.arange(-h_total_point, h_total_point+1)
 
-# @st.cache
-def CR3_trans_func(omega, C=0.01e-6, R=6.8e3):  
-# Transform function for CRx3 circuit
+@st.cache
+def CR3_trans_func(omega, C, R):  
   return (omega*C*R)**3/(((omega*C*R)**3-5*omega*C*R)-1j*(6*(omega*C*R)**2-1))
 
 # def sin_func_gen(fq, h_total_point, time_per_point, C=0.01e-6, R=6.8e3):
