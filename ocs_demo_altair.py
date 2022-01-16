7bc1a8e88ef4c74a0cf2b01be462ae903ba55f17
GeorgeWildboarman <64960316+GeorgeWildboarman@users.noreply.github.com>
2022-01-16 13:12:40 +0900

    Update ocs_demo_altair.py

--- ocs_demo_altair.py
+++ ocs_demo_altair.py
@@ -168,9 +168,11 @@ xgrid_lines = alt.Chart(pf_xgrid).mark_rule(color='white').encode(
 
 # Draw waveforms
 offset = 0
-#  axis=alt.Axis(title=None, grid=True)
 base = alt.Chart(pf_wave).encode(
-    alt.X('x:Q', scale=alt.Scale(domain=xlim)) 
+    alt.X('x:Q', 
+#           axis=alt.Axis(title=None, grid=True), 
+#           scale=alt.Scale(domain=xlim), 
+         ) 
 ).transform_calculate(
     x=alt.datum.x+20
 ).properties(width=550, height=400)
