e2b252012d3deefafe41629fc96ca3cb88265cb2
GeorgeWildboarman <64960316+GeorgeWildboarman@users.noreply.github.com>
2022-01-16 10:58:20 +0900

    Update ocs_demo_altair.py

--- ocs_demo_altair.py
+++ ocs_demo_altair.py
@@ -160,9 +160,18 @@ xgrid_lines = alt.Chart(pf_xgrid).mark_rule(color='white').encode(
 )    
 
 # Draw waveforms
+# base = alt.Chart(pf_wave).encode(
+#     x=alt.X('x:Q', axis=alt.Axis(title=None, grid=True), scale=alt.Scale(domain=xlim)) 
+# ).properties(width=550, height=550)
+
+slider = alt.binding_range(min=-h_total_point//2, max=h_total_point//2, step=1, name='horizontal position')
+selector = alt.selection_single(name='SelectorName', fields=['offset'], bind=slider, init={'offset':20})
+
 base = alt.Chart(pf_wave).encode(
     x=alt.X('x:Q', axis=alt.Axis(title=None, grid=True), scale=alt.Scale(domain=xlim)) 
-).properties(width=550, height=550)
+).add_selection(selector).transform_calculate(
+    x=alt.datum.x + selector.offset
+).properties(width=550, height=400)
 
 line1 = base.mark_line(clip=True, color='orange').encode(
     y=alt.Y('y:Q', scale=alt.Scale(domain=ylim), title='CH1')
