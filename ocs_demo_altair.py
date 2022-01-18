7219754daff823c4da8abde98ca0c75355bec75a
GeorgeWildboarman <64960316+GeorgeWildboarman@users.noreply.github.com>
2022-01-18 12:51:34 +0900

    Update ocs_demo_altair.py

--- ocs_demo_altair.py
+++ ocs_demo_altair.py
@@ -46,7 +46,79 @@ def sin_func_gen(fq, h_total_point, time_per_point, gain, theta):
 
   # Create pandas DF and return it
   return pd.DataFrame({'t':t, 'v1':v1, 'v2':v2, 'label1':['CH1']*len(t), 'label2':['CH2']*len(t)})
-  
+
+@st.cache
+def read_waveform_file(filename='A0000CH1.CSV'):
+  # Read waveform file into Pandas DataFrame
+  # Waveform file format : csv
+  df=pd.read_csv(filename, header=None)
+
+  # Put the data into settup information and waveform
+  # The wave file includes settup information other than waveform data.
+  # Settup info:
+  df_info=df.iloc[:16,].set_index(0)
+  # Waveform data:
+  df_waveform = pd.DataFrame()
+  df_waveform['raw']=pd.Series(df.iloc[16:,0], dtype='int')
+  df_waveform.reset_index(drop=True, inplace=True)
+
+  # Waveform data format
+  # Vertical:
+  # 1division includes 25 points of vertical data.
+  # The vertical point starts from the GND level. 
+  v_points_per_div = 25
+  # 256 points in total (8bit)
+  v_total_points = 256
+  # Vertical Units	: V
+  v_units = str(df_info.loc['Vertical Scale'])
+  # Vertical Scale
+  vol_per_div =float(df_info.loc['Vertical Scale'])
+  vol_per_point = vol_per_div / v_points_per_div
+  # Vertical offset [V]
+  v_offset =float(df_info.loc['Vertical Position'])
+
+  # Horizontal:
+  # 1division includes 250 points of horizontal data.
+  h_points_per_div = 250
+  # 16div in horizontal axis with each 8div from screen center
+  h_div = 16
+  # 4000 points in total
+  h_total_points = h_div * h_points_per_div 
+  # Horizontal Units	: s
+  h_units = str(df_info.loc['Horizontal Scale'])
+  # Horizontal Scale
+  time_per_div =float(df_info.loc['Horizontal Scale'])
+  time_per_point = time_per_div/h_points_per_div
+  # Horizontal offset [s]
+  h_offset =float(df_info.loc['Horizontal Position'])
+
+  # Add practical data
+  df_waveform['time[s]'] = h_point_array(h_total_points)*time_per_point
+  df_waveform['volts[V]'] = df_waveform['raw']*vol_per_point
+
+  return df_waveform
+
+def mmt_waveform(time_per_point=1.0e-6):
+  '''
+  Create pandas DF with 5 columns: t, v1, v2, label1, label2
+  t  : horizontal point in OCS display coordinate 
+  v1 : observed waveform
+  v2 : filled with Not a Number 
+  label1 : Norminal column for fig legend in Altair
+  label2 : Norminal column for fig legend in Altair
+  '''
+  # read waveform file obtained from OSC:DCS-4605 
+  df_waveform = read_waveform_file(filename='A0000CH1.CSV')
+  # Arrange for display  
+  df = pd.DataFrame({
+      't':df_waveform['time[s]']/time_per_point, 
+      'v1':df_waveform['volts[V]'], 
+      'v2':[np.nan]*len(df_waveform), 
+      'label1':['CH1']*len(df_waveform), 
+      'label2':['CH2']*len(df_waveform), 
+      })
+  return df
+
 def div_vals():
   dict_vol ={'5V': 5, '2V': 2, '1V': 1, '500mV': .5, '200mV': .2, '100mV': .1, '50mV': .05, '20mV': .02}
 
@@ -67,12 +139,44 @@ v_point_per_div = 50
 v_total_div = 8
 v_total_point = v_point_per_div * v_total_div
 
-# Create sidebar for FG front panel 
-st.sidebar.title('Function Generator')
 
-# Select params on FG
-fq = st.sidebar.number_input('Frequency [Hz]', value=10000, step=10)
-amp = st.sidebar.number_input('Amp Voltage [V]', value=2, step=1)
+# Create sidbar to draw FG fromt panel
+fg_panel = st.sidebar
+fg_panel.title('Function Generator')
+
+# Add input to set freq and amp
+fq_inp = fg_panel.number_input('Frequency [Hz]', value=10000, step=10)
+amp_inp = fg_panel.number_input('Amp Voltage [V]', value=2, step=1)
+
+# Add radio to select task
+def task_desc_for_radio(task):
+  if 'gen' in task:
+    return 'Wave from funcgen'
+  elif 'file' in task:
+    return 'wavefrom file'
+
+task = fg_panel.radio("Select task", ('funcgen', 'wavefile'), 1, format_func=task_desc_for_radio, key='type')
+# fg_panel.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
+# fg_panel.write(task)
+
+if 'gen' in task:
+  fq = fq_inp
+  amp = amp_inp
+  st.write('task : funcgen')
+elif 'file' in task:
+#   fq = np.nan
+  fq = fq_inp
+  st.write('task : file')
+  amp =1
+
+# ------------------------------------------------------
+# # Create sidebar for FG front panel 
+# st.sidebar.title('Function Generator')
+
+# # Select params on FG
+# fq = st.sidebar.number_input('Frequency [Hz]', value=10000, step=10)
+# amp = st.sidebar.number_input('Amp Voltage [V]', value=2, step=1)
+# ---------------------------------------------------------
 
 # Create OSC Display
 main_dsp = st.container()
