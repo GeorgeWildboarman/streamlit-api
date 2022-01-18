3303ad497dda0fc74a4480e7263336b9c047f9e5
GeorgeWildboarman <64960316+GeorgeWildboarman@users.noreply.github.com>
2022-01-18 11:06:14 +0900

    Update button_trial.py

--- button_trial.py
+++ button_trial.py
@@ -57,6 +57,6 @@ def read_waveform_file(filename='A0000CH1.CSV'):
   return df_waveform
 
 
-task = st.radio("Select task", ('funcgen', 'wavefile'), 1, ['sssssssssssss','ffffffff'])
+task = st.radio("Select task", ('funcgen', 'wavefile'), 1)
 
-
+st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
