4dab277c59fd6351c46b7f8424ad90579c90eb4d
GeorgeWildboarman <64960316+GeorgeWildboarman@users.noreply.github.com>
2022-01-21 14:45:16 +0900

    Update ocs_demo_altair.py

--- ocs_demo_altair.py
+++ ocs_demo_altair.py
@@ -158,7 +158,7 @@ def format_selected_wave(wave):
   if 'sine' in wave:
 #     img = Image.open('/img/Fig_3-1.png')
     img = Image.open('img/Img_3-1.jpg')
-    st.img(image, width=600)
+    st.image(img, width=600)
     return format(' Sine waveform ', '*^26')
   elif 'osc' in wave:
     return format(' Oscillation waveform ', '*^26')
