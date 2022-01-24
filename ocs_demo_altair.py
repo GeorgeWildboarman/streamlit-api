7ce7951fff5d7f65155ab68898064fef1b14ef7e
GeorgeWildboarman <64960316+GeorgeWildboarman@users.noreply.github.com>
2022-01-24 18:32:31 +0900

    Update ocs_demo_altair.py

--- ocs_demo_altair.py
+++ ocs_demo_altair.py
@@ -222,14 +222,14 @@ if 'sine' in selected_wave:
   img2 = Image.open('img/Fig_3-1.png')
   desc_exp.image(img1, width=fig_width)
   desc_exp.image(img2, width=fig_width)
-  desc_exp.markdown('
+  desc_exp.markdown('''
   FGの出力を実験用CR移相回路の入力に加え，その電圧信号をオシロスコープのCH1で観測する．出力はオシロスコープのCH2で観測する．<br>
   CH1とCH2の信号を測定しやすいように，横軸と縦軸のスケールを調整して描画すること．<br><br>
   FGから以下の波形を出力させ，画像を作成する．<br>
   -波形: 正弦波<br>
   -振幅: 2V<br>
-  -周波数: 10kHz，8kHz，6kHz，5kHz，4kHz，3kHz，2kHz，1kHz, 800Hz，600Hz，500Hz<br>
-  ', unsafe_allow_html=True)
+  -周波数: *10kHz，8kHz，6kHz，5kHz，4kHz，3kHz，2kHz，1kHz, 800Hz，600Hz，500Hz* <br>
+  ''', unsafe_allow_html=True)
 elif 'osc' in selected_wave:
   desc_exp.subheader('CR移相型発振回路の発振周波数の測定')
   img1 = Image.open('img/Img_3-2.jpg')
